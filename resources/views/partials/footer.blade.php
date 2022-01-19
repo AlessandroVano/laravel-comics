37239b1378319021798e22ab82b91a044ed94f4f
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-19 22:35:55 +0100

    insert footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,39 @@
-<h1>footer</h1>
+<section class="bg">
+        <div class="container  content">
+        <button class="btn-2">
+            <h1>SIGN-UP NOW! </h1> 
+        </button>
+        <nav>
+            <ul class="padding">
+                <li>
+                    <h2>FOLLOW US</h2>
+                </li>
+                <li>
+                    <a href="/">
+                    <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                    <img src="{{ asset('images/footer-periscope.png')}}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                    <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                    <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                    <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+</section>
